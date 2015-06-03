7b888ec00cf7f59493ae845488138e08b82cf7cc
diegojfdez <diego.j.fdez@gmail.com>
2015-06-03 18:45:59 +0200

    Desarrollo página autores

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET /author. */
+router.get('/author', function(req, res) {
+  res.render('author', { autor: 'Diego J. Fernández Raposo' });
+});
+
 /* controlador de las preguntas */
 router.get('/quizes/question', quizController.question);
 
